0be966089147fdf5985b5f5a049f2f475db10f9d
moisedk <moisedete@gmail.com>
2024-07-18 00:45:16 -0400

    Add colors for light and dark themes

--- app/src/main/java/com/github/fric/ui/theme/Color.kt
+++ app/src/main/java/com/github/fric/ui/theme/Color.kt
@@ -1,4 +1,22 @@
 package com.github.fric.ui.theme
 
-class Colors {
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.material.Colors
+
+class Color
+{
+    val md_theme_light_onPrimary = Color(0xFFFFF59D)
+    val md_theme_light_primary = Color(0xFFFFEB3B)
+    val md_theme_light_secondary = Color(0xFF388E3C)
+    val md_theme_light_onSecondary = Color(0xFFA5D6A7)
+    val md_theme_light_primaryContainer = Color(0xFFFBC02D)
+
+
+    val md_theme_dark_onSecondary = Color(0xFFF48FB1)
+    val md_theme_dark_primary = Color(0xFF251E00)
+    val md_theme_dark_onPrimary = Color(0xFF211316)
+    val md_theme_dark_onPrimaryContainer = Color(0xFFC2185B)
+    val md_theme_dark_secondary = Color(0xFF1B1F15)
+
 }
