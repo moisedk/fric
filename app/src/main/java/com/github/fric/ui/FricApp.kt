ec29b5ae9cd1f316c2c3b90218e2616e1bf2d90f
moisedk <moisedete@gmail.com>
2024-07-20 20:20:57 -0400

    Add FricApp constructor Add Window state configuration detection utility functions

--- app/src/main/java/com/github/fric/ui/FricApp.kt
+++ app/src/main/java/com/github/fric/ui/FricApp.kt
@@ -1,23 +1,72 @@
 package com.github.fric.ui
 
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteType
+import androidx.compose.material3.windowsizeclass.WindowSizeClass
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import androidx.window.layout.DisplayFeature
+import androidx.window.layout.FoldingFeature
+import com.github.fric.ui.navigation.FricNavActions
 import com.github.fric.ui.navigation.FricRoute
+import com.github.fric.ui.utils.DevicePosture
 import com.github.fric.ui.utils.FricContentType
 import com.github.fric.ui.utils.FricNavType
+import com.github.fric.ui.utils.isBookPosture
+import com.github.fric.ui.utils.isSeparating
 
-    // Maps FricApp Navigation type to NavigationSuiteType for adaptive navigation suite
-   private  fun NavigationSuiteType.toNavType() = when (this) {
-       NavigationSuiteType.NavigationBar -> FricNavType.BOTTOM_NAVIGATION
-       NavigationSuiteType.NavigationRail -> FricNavType.NAVIGATION_RAIL
-       NavigationSuiteType.NavigationDrawer -> FricNavType.PERMANENT_NAVIGATION_DRAWER
-       else -> FricNavType.BOTTOM_NAVIGATION
+// Maps FricApp Navigation type to NavigationSuiteType for adaptive navigation suite
+private fun NavigationSuiteType.toNavType() = when (this) {
+    NavigationSuiteType.NavigationBar -> FricNavType.BOTTOM_NAVIGATION
+    NavigationSuiteType.NavigationRail -> FricNavType.NAVIGATION_RAIL
+    NavigationSuiteType.NavigationDrawer -> FricNavType.PERMANENT_NAVIGATION_DRAWER
+    else -> FricNavType.BOTTOM_NAVIGATION
+}
+
+@Composable
+public fun FricApp(
+    windowSize: WindowSizeClass,
+    fricHomeUIState: FricHomeUIState,
+    fricNavType: FricNavType,
+    displayFeatures: List<DisplayFeature>,
+    closeRecordScreen: () -> Unit = {},
+    navigateToRecord: (Long, FricContentType) -> Unit = { _, _ -> },
+) {
+    val foldingFeature = displayFeatures.filterIsInstance<FoldingFeature>().firstOrNull()
+
+    val foldingDevicePosture = when {
+        isBookPosture(foldingFeature) ->
+            DevicePosture.BookPosture(foldingFeature.bounds)
+
+        isSeparating(foldingFeature) ->
+            DevicePosture.Separating(foldingFeature.bounds, foldingFeature.orientation)
+
+        else -> DevicePosture.NormalPosture
+
     }
+    val contentType = when (windowSize.widthSizeClass) {
+        WindowWidthSizeClass.Compact -> FricContentType.SINGLE_PANE
+        WindowWidthSizeClass.Medium -> if (foldingDevicePosture != DevicePosture.NormalPosture) {
+            FricContentType.TWO_PANE
+        } else {
+            FricContentType.SINGLE_PANE
+        }
+        WindowWidthSizeClass.Expanded -> FricContentType.TWO_PANE
+        else -> FricContentType.SINGLE_PANE
+    }
+    val navController = rememberNavController()
+    val navActions = remember(navController) { FricNavActions(navController) }
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentDestination = navBackStackEntry?.destination?.route ?: FricRoute.OVERVIEW
+}
 
 @Composable
 private fun FricNavHost(
@@ -27,7 +76,7 @@ private fun FricNavHost(
     fricNavType: FricNavType,
     displayFeatures: List<DisplayFeature>,
     closeRecordScreen: () -> Unit,
-    navigateToRecord:  (Long, FricContentType) -> Unit,
+    navigateToRecord: (Long, FricContentType) -> Unit,
     modifier: Modifier = Modifier
 ) {
     NavHost(
--- app/src/main/java/com/github/fric/ui/utils/WindowStateUtils.kt
+++ app/src/main/java/com/github/fric/ui/utils/WindowStateUtils.kt
@@ -1,4 +1,14 @@
 package com.github.fric.ui.utils
+/*
+ * This code was adapted from the Android Open Source Project Sample from
+ * @link https://github.com/android/compose-samples/blob/main/Reply/app/src/main/java/com/example/reply/ui/ReplyApp.kt
+ *
+ */
+
+import android.graphics.Rect
+import androidx.window.layout.FoldingFeature
+import kotlin.contracts.ExperimentalContracts
+import kotlin.contracts.contract
 
 
 // Adaptive Navigation Suite
@@ -14,4 +24,29 @@ enum class FricContentType {
 enum class FricNavContentPosition {
     TOP,
     CENTER
+}
+sealed interface DevicePosture {
+    data object NormalPosture : DevicePosture
+
+    data class BookPosture(
+        val hingePosition: Rect
+    ) : DevicePosture
+
+    data class Separating(
+        val hingePosition: Rect,
+        var orientation: FoldingFeature.Orientation
+    ) : DevicePosture
+}
+
+@OptIn(ExperimentalContracts::class)
+fun isBookPosture(foldFeature: FoldingFeature?): Boolean {
+    contract { returns(true) implies (foldFeature != null) }
+    return foldFeature?.state == FoldingFeature.State.HALF_OPENED &&
+            foldFeature.orientation == FoldingFeature.Orientation.VERTICAL
+}
+
+@OptIn(ExperimentalContracts::class)
+fun isSeparating(foldFeature: FoldingFeature?): Boolean {
+    contract { returns(true) implies (foldFeature != null) }
+    return foldFeature?.state == FoldingFeature.State.FLAT && foldFeature.isSeparating
 }
