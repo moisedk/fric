2794ec78b89dc3b6a4844dc46806ff592f330675
moisedk <moisedete@gmail.com>
2024-08-20 17:26:41 -0400

    Implement login and authentication

--- app/src/main/java/com/github/fric/MainActivity.kt
+++ app/src/main/java/com/github/fric/MainActivity.kt
@@ -3,45 +3,65 @@ package com.github.fric
 import android.os.Bundle
 import android.os.SystemClock
 import androidx.activity.ComponentActivity
-import androidx.activity.enableEdgeToEdge
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
-import androidx.compose.runtime.getValue
 import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
 import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
-import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.github.fric.data.viewModels.BudgetViewModel
 import com.github.fric.data.viewModels.FricHomeViewModel
 import com.github.fric.ui.FricApp
+import com.github.fric.ui.LoginScreen
 import com.github.fric.ui.theme.AppTheme
 import com.google.accompanist.adaptive.calculateDisplayFeatures
-
 
 class MainActivity : ComponentActivity() {
     private val homeViewModel: FricHomeViewModel by viewModels()
     private val fricViewModel: BudgetViewModel by viewModels()
+
     @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContent {
-            AppTheme {
-                val windowSize = calculateWindowSizeClass(this)
-                val displayFeatures = calculateDisplayFeatures(this)
-                val homeUiState by homeViewModel.uiState.collectAsStateWithLifecycle()
-                val fricUiState by fricViewModel.uiState.collectAsStateWithLifecycle()
+            var isUserLoggedIn by remember { mutableStateOf(false) }
+            if (isUserLoggedIn) {
+                AppTheme {
+                    val windowSize = calculateWindowSizeClass(this)
+                    val displayFeatures = calculateDisplayFeatures(this)
+                    val homeUiState by homeViewModel.uiState.collectAsStateWithLifecycle()
 
-                FricApp(
-                    windowSize = windowSize,
-                    displayFeatures = displayFeatures,
-                    fricHomeUIState = homeUiState,
-                    fricBudgetUIState = fricUiState,
-                    closeRecordScreen = {
-                        homeViewModel.closeRecordScreen()
-                    },
-                    navigateToRecord = { expenseId, pane ->
-                        homeViewModel.openReportExpenseScreen(expenseId, pane)
+                    FricApp(
+                        windowSize = windowSize,
+                        displayFeatures = displayFeatures,
+                        fricHomeUIState = homeUiState,
+                        budgetViewModel = fricViewModel,
+                        closeRecordScreen = {
+                            homeViewModel.closeRecordScreen()
+                        },
+                        navigateToRecord = { expenseId, pane ->
+                            homeViewModel.openReportExpenseScreen(expenseId, pane)
+                        }
+                    )
+                }
+            }
+            else {
+                // Login Screen
+                LoginScreen(
+                    onRegisterClick = { /*TODO*/ },
+                    onForgotPasswordClick = { /*TODO*/ },
+                    onLoginSuccess = { /*TODO*/},
+                    onLoginFailure = {/*TODO*/},
+                    onLoginClick = {
+                        username, password ->
+                        homeViewModel.login(username, password) { loginResult ->
+                            isUserLoggedIn = loginResult
+                        }
                     }
                 )
             }
---
+++ app/src/main/java/com/github/fric/ui/FricLogin.kt
@@ -0,0 +1,135 @@
+package com.github.fric.ui
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.github.fric.R
+
+@Composable
+fun LoginScreen(
+    modifier: Modifier = Modifier,
+    onRegisterClick: () -> Unit,
+    onForgotPasswordClick: () -> Unit,
+    onLoginSuccess: () -> Unit,
+    onLoginFailure: () -> Unit,
+    onLoginClick: (username: String, password: String) -> Unit
+) {
+    var username: String by remember { mutableStateOf("") }
+    var password: String by remember { mutableStateOf("") }
+
+    Box(modifier = modifier.fillMaxSize()) {
+        // Background image
+        Image(
+            painter = painterResource(id = R.drawable.fric_bg), // Replace with your image
+            contentDescription = "Background",
+            modifier = Modifier.fillMaxSize(), contentScale = ContentScale.Crop
+        )
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            // Username TextField
+            OutlinedTextField(
+                value = username, // Replace with your state variable
+                onValueChange = { username = it.trim() },
+                label = { Text("Username") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 48.dp)
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))// Password TextField
+            OutlinedTextField(
+                value = password, // Replace with your state variable
+                onValueChange = { password = it},
+                label = { Text("Password") },
+                visualTransformation = PasswordVisualTransformation(),
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            // Forgot Password Text
+            Text(
+                text = "Forgot Password?",
+                modifier = Modifier
+                    .clickable { onForgotPasswordClick() }
+                    .align(Alignment.End),
+                color = Color.Blue,
+                style = MaterialTheme.typography.bodyMedium
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            // Login Button
+            Button(
+                onClick = { onLoginClick(username, password)},
+                colors = ButtonColors(
+                    contentColor = MaterialTheme.colorScheme.onPrimary,
+                    containerColor = MaterialTheme.colorScheme.primary,
+                    disabledContainerColor = Color.Transparent,
+                    disabledContentColor = Color.Transparent
+                )
+            ) {
+                Text("Login")
+            }
+        }
+
+        // Register Button
+        Button(
+            onClick = { onRegisterClick() },
+            modifier = Modifier
+                .align(Alignment.TopEnd)
+                .padding(16.dp),
+            colors = ButtonColors(
+                contentColor = MaterialTheme.colorScheme.primary,
+                containerColor = Color.Transparent,
+                disabledContainerColor = Color.Transparent,
+                disabledContentColor = Color.Transparent
+            )
+        ) {
+            Text("Register")
+        }
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun LoginScreenPreview() {
+    LoginScreen(
+        onRegisterClick = {},
+        onForgotPasswordClick = {},
+        onLoginSuccess = {},
+        onLoginFailure = {},
+        onLoginClick = {_, _, ->}
+    )
+}
